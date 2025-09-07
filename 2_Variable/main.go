e6affa01a1c31a681027401731e70e89c49b7146
Raj Yadav <raj819314@gmail.com>
2025-09-08 01:43:58 +0530

    add variable declaration examples in main.go

---
+++ 2_Variable/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	// Variable declaration and initialization
+	// var name = "Raj"
+	// Short variable declaration
+	// name := "Raj"
+	// println("Good morning,", name)
+
+	// Variable with explicit type
+	// var flowerName string = "Rose"
+	// fmt.Println("Flower name is:", flowerName)
+	// fmt.Printf("Type of flowerName is: %T\n", flowerName)
+
+	// Default value of variable
+	// var age int
+	// fmt.Println("Default value of age is:", age)
+
+	// Multiple variable declaration
+	// var firstName, lastName string = "Raj", "Yadav"
+	// println("First Name:", firstName)
+	// println("Last Name:", lastName)
+}
